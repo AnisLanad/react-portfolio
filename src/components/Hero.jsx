308cd9a19e6d5cba83bedf6bdb49cd900decad93
anis <ma_lanad@esi.dz>
2024-10-05 21:38:31 +0100

    Fix case sensitivity issue with Technologies component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion"
 import { HERO_CONTENT } from "../constants"
-import profilePic from "../assets/KevinRushProfile.png"
+import profilePic from "../assets/kevinRushProfile.png"
 
 const container = (delay) => ({
     hidden: {x: -100, opacity: 0},
